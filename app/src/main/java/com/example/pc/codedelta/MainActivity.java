d7a2251b16981a834fc7358df60d3b83585617e8
sparky-to-pseudo <ittakesaspark@gmail.com>
2017-11-10 16:22:52 -0600

    new test

--- app/src/main/java/com/example/pc/codedelta/MainActivity.java
+++ app/src/main/java/com/example/pc/codedelta/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // some testing comment
 
 
     }
