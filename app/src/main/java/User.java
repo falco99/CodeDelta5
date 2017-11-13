6c35f03cc067d296d5fc53f15da46bc25fe20877
jared69 <jared.gilstrap@mavs.uta.edu>
2017-11-12 18:12:00 -0600

    updated some stuff in user class

---
+++ app/src/main/java/User.java
@@ -0,0 +1,64 @@
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(primaryKeys = {"firstName", "lastName"})
+public class User {
+    @PrimaryKey
+    private int uid;
+
+    @ColumnInfo(name = "first_name")
+    private String firstName;
+
+    @ColumnInfo(name = "last_name")
+    private String lastName;
+
+    @ColumnInfo(name = "Username")
+    private String userName;
+
+    @ColumnInfo(name = "Password")
+    private String passWord;
+
+    @ColumnInfo(name = "Email")
+    private String emailAdd;
+
+    public User(String first, String last, String userN, String passW, String mailad){
+        this.firstName = first;
+        this.lastName = last;
+        this.userName = userN;
+        this.passWord = passW;
+        this.emailAdd = mailad;
+    }
+
+    // getting user
+    public String getuser(){
+        return this.userName;
+    }
+
+    // setting user
+    public void setuser(String userN){
+        this.userName = userN;
+    }
+
+    // getting Pass
+    public String getPass(){
+        return this.passWord;
+    }
+
+    // setting pass
+    public void setName(String passW){
+        this.passWord = passW;
+    }
+
+    // getting email
+    public String getmail(){
+        return this.emailAdd;
+    }
+
+    // setting pass
+    public void setmail(String mail){
+        this.emailAdd = mail;
+    }
+
+
+}
