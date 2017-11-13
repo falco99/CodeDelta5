fa79a0e269265212a9575d5d9ad9ef90d00e8e3a
santoshnpl54 <santoshnpl54@gmail.com>
2017-11-13 03:01:20 -0600

    Login started plus

--- app/src/main/java/com/example/pc/codedelta/RegisterActivity.java
+++ app/src/main/java/com/example/pc/codedelta/RegisterActivity.java
@@ -1,28 +1,23 @@
 package com.example.pc.codedelta;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
+public class RegisterActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // some testing comment
-        //gradle update check
-       final EditText etFname = findViewById(R.id.etFname);
-        final EditText etLname =  findViewById(R.id.etLname);
+        setContentView(R.layout.activity_register);
+        final EditText etFname = findViewById(R.id.etFname);
+        final EditText etLname = findViewById(R.id.etLname);
         final EditText etPhone = findViewById(R.id.etPhone);
-        final EditText etUsername =  findViewById(R.id.etUsername);
+        final EditText etUsername = findViewById(R.id.etUsername);
+        final EditText etPassword = findViewById(R.id.etPassword);
         final EditText etDob = findViewById(R.id.etDob);
-
-        final Button bLogout = findViewById(R.id.bLogout);
-
-
+        final Button bRegister = findViewById(R.id.bRegister);
     }
 }
