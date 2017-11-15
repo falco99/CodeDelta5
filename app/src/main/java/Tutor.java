737e2bf1121a6bc6665419d7b3bc6362f21a1e0f
sparky-to-pseudo <ittakesaspark@gmail.com>
2017-11-15 11:25:08 -0600

    Adding tutor class.

---
+++ app/src/main/java/Tutor.java
@@ -0,0 +1,25 @@
+/**
+ * Created by ittak on 11/13/2017.
+ */
+
+/* Tutor Class is used to store the tutor's information.
+ * Locations, Levels, and Categories that will assist in
+ * searching for the correct tutor in tutoring categories,
+ * while the rest of the data can be used for display
+ * and or search purposes.
+ */
+public class Tutor {
+
+    // Will provide getters and setters.
+
+    private String Locations[];
+    private String Levels[];
+    private String Categories[];
+    private String Fname;
+    private String Lname;
+    private String Bio;
+    private boolean tutor;//if 1 tutor, if 0 not
+    private boolean user;// if 1 user, if 0 not
+    // private ProfilePicture, need to figure out how to add an image for the tutor's profile picture.
+
+}
