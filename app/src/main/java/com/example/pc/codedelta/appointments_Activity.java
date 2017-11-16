7bba3ff3394150ae4a1f5d105ac8447ed4f55834
falco99 <coryfalvo1@gmail.com>
2017-11-15 22:47:21 -0600

    a

---
+++ app/src/main/java/com/example/pc/codedelta/appointments_Activity.java
@@ -0,0 +1,13 @@
+package com.example.pc.codedelta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class appointments_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointments_);
+    }
+}
