0a8df08e1862e02cc842c88df8973235c2dc7cc3
santoshnpl54 <santoshnpl54@gmail.com>
2017-11-17 19:42:05 -0600

    reg activity modified

---
+++ app/src/main/java/com/example/pc/codedelta/MySqliteOpenHelper.java
@@ -0,0 +1,41 @@
+package com.example.pc.codedelta;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by z on 11/17/17.
+ */
+
+public class MySqliteOpenHelper extends SQLiteOpenHelper
+{
+    private static final String database_name ="tutorapp.db";
+    private String table_name = "people";
+    private static final int database_version = 1;
+
+    private String column_id = "id",column_Fname ="Fname",column_Lname ="LName",column_Dob = "Dob",
+    column_phone ="Phone",column_username ="username",column_password = "password";
+
+   private String database_create_statement =
+           "create table"+ table_name+"("+column_id +" integer primary key autoincrement, "+column_Fname+"text not null" +column_Lname+"text not null"+
+           column_Dob+"text not null"+column_phone+"text not null"+column_username+"text not null" +column_password+ "text not null";
+
+
+   public MySqliteOpenHelper(Context context){
+       super(context,database_name,null,database_version);
+
+   }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase)
+    {
+     sqLiteDatabase.execSQL(database_create_statement);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
+}
--- app/src/main/java/com/example/pc/codedelta/RegisterActivity.java
+++ app/src/main/java/com/example/pc/codedelta/RegisterActivity.java
@@ -1,6 +1,10 @@
 package com.example.pc.codedelta;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +14,8 @@ import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity{
 
+    private MySqliteOpenHelper mySqliteOpenHelper;
+    private SQLiteDatabase mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +29,20 @@ public class RegisterActivity extends AppCompatActivity{
         final Button bRegister = findViewById(R.id.bRegister);
 
         final TextView tvCancel = findViewById(R.id.tvCancel);
+
+
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mySqliteOpenHelper = new MySqliteOpenHelper(getApplicationContext());
+                mDatabase = mySqliteOpenHelper.getWritableDatabase();
+
+                ContentValues values = new ContentValues();
+
+
+                mDatabase.insert("table_Name", null, values);
+
+
                 Intent Reg = new Intent(RegisterActivity.this,LoginActivity.class);
                 RegisterActivity.this.startActivity(Reg);
 
