fca3fc78d2465abdefd3c76f2186b183eb40db55
falco99 <coryfalvo1@gmail.com>
2017-11-19 11:40:04 -0600

    Update

--- app/src/main/java/com/example/pc/codedelta/TutorList.java
+++ app/src/main/java/com/example/pc/codedelta/TutorList.java
@@ -2,12 +2,73 @@ package com.example.pc.codedelta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class TutorList extends AppCompatActivity {
+    private static final String TAG = "TutorList";
+
+    int[] images = {R.drawable.chrisprofile,R.drawable.trevorcartoonprofile,R.drawable.shelly,R.drawable.chrisprofile};
+    String[] names = {"Chris","Trevor","Shelly","Chris"};
+    String[] dates = {"Monday 12:00pm Arlington","Tuesday 12:00pm Arlington","Wednesday 12:00pm Arlington","Thursday 12:00pm Arlington"};
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor_list);
+        Log.d(TAG, "onCreate: Started.");
+        list = findViewById(R.id.listview);
+
+        CustomAdapter customAdapter = new CustomAdapter();
+
+        list.setAdapter(customAdapter);
+
     }
+
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View view = getLayoutInflater().inflate(R.layout.customlayout, null);
+
+            ImageView mImageView = view.findViewById(R.id.imageView2);
+            TextView mNameView = view.findViewById(R.id.Name);
+            TextView mDateView = view.findViewById(R.id.Date);
+
+            mImageView.setImageResource(images[position]);
+            mNameView.setText(names[position]);
+            mDateView.setText(dates[position]);
+            return view;
+        }
+    }
+
+
+
+
 }
+
