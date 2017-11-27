ad1d1a77f79d9184455f3e07f6d27a9eea1fbf00
falco99 <coryfalvo1@gmail.com>
2017-11-27 14:04:30 -0600

    Update

--- app/src/main/java/com/example/pc/codedelta/CodeDelta5.java
+++ app/src/main/java/com/example/pc/codedelta/CodeDelta5.java
@@ -14,6 +14,11 @@ public class CodeDelta5 extends Application {
     public String security_No;
     public String ccMonth;
     public String ccYear;
+    public String[] names;
+    public int[] images;
+    public String[] dates;
+
+
 
 
     public CodeDelta5() {
@@ -27,6 +32,9 @@ public class CodeDelta5 extends Application {
         security_No = "302";
         ccMonth = "8";
         ccYear = "17";
+        images = new int[]{R.drawable.chrisprofile, R.drawable.trevorcartoonprofile, R.drawable.shelly, R.drawable.chrisprofile};
+        names = new String[]{"Chris", "Trevor", "Shelly", "Chris"};
+        dates = new String[]{"Monday 12:00pm Arlington", "Tuesday 12:00pm Arlington", "Wednesday 12:00pm Arlington", "Thursday 12:00pm Arlington"};
 
 
 
