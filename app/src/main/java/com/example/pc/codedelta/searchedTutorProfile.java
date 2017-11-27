ec63723ba326ef0189b3a0e862a6dc74206a771d
sparky-to-pseudo <ittakesaspark@gmail.com>
2017-11-27 15:59:05 -0600

    passing values to tutor searched profile.

--- app/src/main/java/com/example/pc/codedelta/searchedTutorProfile.java
+++ app/src/main/java/com/example/pc/codedelta/searchedTutorProfile.java
@@ -1,7 +1,10 @@
 package com.example.pc.codedelta;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class searchedTutorProfile extends AppCompatActivity {
 
@@ -9,6 +12,28 @@ public class searchedTutorProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searched_tutor_profile);
+
+        Intent i = getIntent();
+
+        String firstName = i.getStringExtra("name");
+        String lastName = i.getStringExtra("last");
+
+        TextView textFirst = findViewById(R.id.first);
+        TextView textLast = (TextView) findViewById(R.id.last);
+
+        textFirst.setText(firstName);
+        textLast.setText(lastName);
+
+     /*   TextView myAwesomeTextView = (TextView)findViewById(R.id.myAwesomeTextView)
+//in your OnCreate() method
+        myAwesomeTextView.setText("My Awesome Text")  */
+
+
+        Toast.makeText(this, firstName,
+                Toast.LENGTH_LONG).show();
+
+        Toast.makeText(this, lastName,
+                Toast.LENGTH_LONG).show();
 
     }
 
